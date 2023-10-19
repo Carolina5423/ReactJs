e7bb9e14586e6e2780423dfdeb7cda1042ea0149
Carolina5423 <carolina.olmoss@gmail.com>
2023-10-18 23:09:56 -0300

    primer comimit

---
+++ OneDrive/Desktop/react-curso/src/App.js
@@ -0,0 +1,12 @@
+
+import Navbar from "./components/Naavbar/Navbar";
+
+function App() {
+  return (
+  <div>
+
+  </div>
+  );
+}
+
+export default App;
---
+++ OneDrive/Desktop/react-curso/src/components/Naavbar/Navbar.jsx
@@ -0,0 +1,21 @@
+
+import logoSostre from './public/logosostre.jpg'
+const Navbar = () => {
+
+    return {
+        <div>
+           <div>
+           <img alt="sostre"  src=logoSostre} />
+           <h1> SOSTRE </h1>
+        
+           </div>
+
+        <div>
+        
+        </div>
+    
+        </div>
+    }
+};
+
+export default Navbar;
